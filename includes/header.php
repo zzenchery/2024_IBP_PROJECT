d77cf348dffb7a07f8d25a6b428b08a488e61bb3
zeynepaydogan <zy.aydogan@gmail.com>
2024-05-31 15:16:22 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
+    <meta name="comment" content="">
     <meta name="director" content="">
     <title>Administrator</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
